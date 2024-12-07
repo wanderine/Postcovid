3e1ae69a286bec610fb51964f882c531af25b81d
Anders Eklund <andek034@gmail.com>
2024-12-07 23:33:49 +0100

    Create run_dualregression.sh

---
+++ fMRI/run_dualregression.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dual_regression slicetiming_40components.gica/groupmelodic.ica/melodic_IC 1 Analyses/group_covariates.mat Analyses/group_covariates.con 5000 DRgroup_covariates_slicetiming_40components `cat slicetiming_40components.gica/.filelist` 
+
+
